1275465514ad92b3ff55d422dd6ea88889b8b542
au5xi <henteri.hallamaa@gmail.com>
2026-01-28 18:15:37 +0200

    Delete server directory

--- server/src/utils/date.ts
+++
@@ -1,7 +0,0 @@
-export function dayBucketUTC(date: Date) {
-  return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()));
-}
-
-export function addDaysUTC(date: Date, days: number) {
-  return new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
-}
